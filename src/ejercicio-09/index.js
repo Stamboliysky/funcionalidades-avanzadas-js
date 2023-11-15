567d4f1f50ad0d8954c4dfaf6ea0eb306fdfac42
Stamboliysky <130237149+Stamboliysky@users.noreply.github.com>
2023-11-15 12:42:04 +0100

    Revert "Añadir ejercicio-09"

--- src/ejercicio-09/index.js
+++ src/ejercicio-09/index.js
@@ -1,14 +1,1 @@
-// Ejercicio-09: Uso de Set
-
-const colors = new Set()
-
-colors.add('Rojo')
-colors.add('Azul')
-colors.add('Verde')
-colors.add('Rojo')
-
-console.log('Colores en Set:',colors)
-
-const colorTocheck = 'Verde'
-console.log(`¿${colorTocheck} existe en Set? 
-${colors.has(colorTocheck)}`)
+// Escribe tu código aquí:
